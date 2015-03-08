339564436ab8f9cb2a5e1530b612b6e3ada8b3a6
Carlos Rosquillas <crosquillas@gmail.com>
2015-03-07 19:48:23 -0600

    Add XML read/write skeleton

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
 'use strict';
 
-var Q = require('q');
+var fs     = require('fs');
+var xml2js = require('xml2js');
 
-module.exports = function () {
-  var dfd = Q.defer();
-  dfd.resolve();
+module.exports = function (context) {
+  var Q       = context.requireCordovaModule('q');
+  var dfd     = new Q.defer();
+  var parser  = new xml2js.Parser();
+  var builder = new xml2js.Builder();
+
+  var output;
+  var path;
+
+  var manifestExists = false;
+
+  try {
+    path = context.opts.projectRoot + '/config.xml';
+    manifestExists = fs.existsSync(path);
+  } catch (error) {
+    dfd.reject();
+  }
+
+  if (manifestExists) {
+    fs.readFile(path, function(err, data) {
+      parser.parseString(data, function(err, result) {
+        var modified = result;
+
+        function callback(err) {
+          if (err) {
+            console.log(err);
+            dfd.reject();
+          } else {
+            console.log('Updating "com.disusered.stetho" AndroidManifest.xml');
+            dfd.resolve();
+          }
+        }
+
+        try {
+          // TODO: update config.xml
+          output = builder.buildObject(modified);
+          fs.writeFile(path, output, callback);
+        } catch (error) {
+          dfd.reject();
+        }
+      });
+    });
+  } else {
+    dfd.reject();
+  }
+
   return dfd.promise;
 };
