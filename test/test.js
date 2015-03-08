d3b1f4da876628f4d57478c4bd331170b9cb270a
Carlos Rosquillas <crosquillas@gmail.com>
2015-03-07 22:43:45 -0600

    Fix test

--- test/test.js
+++ test/test.js
@@ -1,12 +1,23 @@
 'use strict';
 var assert = require('assert');
-var cordovaVersion = require('../');
+var version = require('../');
 
 describe('cordova-version node module', function () {
-  it('must return a promise', function() {
-    var cdv = cordovaVersion();
-    assert(typeof cdv.then === 'function', 'Return value is not thenable');
-    assert(typeof cdv.done === 'function', 'Return value has no done method');
-    assert(typeof cdv.catch === 'function', 'Return value has no catch method');
+  it('must return an object', function() {
+    assert(typeof version === 'object', 'Return value is not an object');
+  });
+
+  describe('cordova-version api - update', function () {
+    it('must exist', function() {
+      var up = version.update();
+      assert(up, 'Update method does not exist');
+    });
+
+    it('must return a promise', function() {
+      var up = version.update();
+      assert(typeof up.then === 'function', 'Update method is not thenable');
+      assert(typeof up.done === 'function', 'Update method has no done method');
+      assert(typeof up.catch === 'function', 'Update method has no catch method');
+    });
   });
 });
